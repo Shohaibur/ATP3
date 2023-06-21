f9f32fbbd5e03edd8df0fc49197c6a214edb4dd0
shohaibur <shoaibrochi9@gmail.com>
2023-06-21 21:19:02 +0600

    added user decorator.

--- Back-end/src/app.controller.ts
+++ Back-end/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+let User=[];
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+ 
+  @Get('user')
+  getUsers(){
+    return User;
   }
 }
